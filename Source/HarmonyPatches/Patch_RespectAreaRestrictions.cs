35b87697de99ccd8b3921a4913ad04416d590a5c
Akano <61079646+AkanoQWQ@users.noreply.github.com>
2026-05-08 00:23:38 +0800

    Fix bug about RespectRestrictions

---
+++ Source/HarmonyPatches/Patch_RespectAreaRestrictions.cs
@@ -0,0 +1,18 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using RimPrison.Core;
+
+namespace RimPrison.HarmonyPatches
+{
+    // Labor-enabled prisoners should respect allowed area restrictions.
+    [HarmonyPatch(typeof(ForbidUtility), "IsForbidden", typeof(IntVec3), typeof(Pawn))]
+    public static class Patch_RespectAreaRestrictions
+    {
+        static void Postfix(IntVec3 c, Pawn pawn, ref bool __result)
+        {
+            if (!__result && pawn.IsLaborEnabled() && !c.InAllowedArea(pawn))
+                __result = true;
+        }
+    }
+}
